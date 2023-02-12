3815196c6991bd0b4d25af1457e1c8d845763ca7
shahdhairya51 <shahdhairya51@gmail.com>
2023-02-12 10:06:45 +0530

    final final

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, request, jsonify ,redirect, url_for
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
 from io import BytesIO
 import json
 import base64
-""" from tensorflow.keras.models import load_model
+from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
 from tensorflow.keras.preprocessing.image import img_to_array
-import numpy as np """
+import numpy as np
 
 
 app = Flask(__name__)
@@ -20,14 +20,15 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def home():
     return render_template('index.html')
 
+
 @app.route('/test', methods=['GET'])
 def test():
     return render_template('uploader/index.html')
 
+
 @app.route('/upload', methods=['GET'])
 def upload():
     return render_template('upload.html')
-
 
 
 @app.route('/uploadfile', methods=['POST'])
@@ -35,18 +36,16 @@ def uploadfile():
     if request.method == 'POST':
         file = request.files['file']
         file.save(os.path.join(app.root_path, f"uploads/{file.filename}"))
-        img_path=os.path.join(app.root_path, f"uploads/{file.filename}")
+        img_path = os.path.join(app.root_path, f"uploads/{file.filename}")
         print(img_path)
-        result="Good"
-        
-        return ""
-        #return redirect(url_for('esha_card/pg2.htmldex'))
-        #render_template('esha_card/pg2.html',result = result,img_path=img_path)
-        """
+
+        # return ""
+        # return redirect(url_for('esha_card/pg2.htmldex'))
+        # render_template('esha_card/pg2.html',result = result,img_path=img_path)
+
         model = load_model('model2_resnet.h5')
-        
-        
-         img = image.load_img(img_path, target_size=(224, 224))
+
+        img = image.load_img(img_path, target_size=(224, 224))
 
         imgResult = img_to_array(img)
         imgResult = np.expand_dims(imgResult, axis=0)
@@ -62,9 +61,7 @@ def uploadfile():
         # print the label of the class with maximum score
         print(class_labels[pred[0]])
 
-        return class_labels[pred[0]] 
-        return """
-    
+        return class_labels[pred[0]]
 
 
 @app.route('/receivedata', methods=['POST'])
@@ -72,9 +69,11 @@ def receive():
     print(request.form['myData'])
     return ''
 
+
 @app.route('/cards', methods=['GET'])
 def cards():
-    return render_template('esha_card/pg2.html',result = "Hello",img_path="/Users/droom/Documents/newunscript/uploads/Very Bad Images[Accuracy_75]260.png")
+    return render_template('esha_card/pg2.html', result="Hello", img_path="/Users/droom/Documents/newunscript/uploads/Very Bad Images[Accuracy_75]260.png")
+
 
 @app.route("/example", methods=["POST"])
 def example():
@@ -82,8 +81,8 @@ def example():
     data = request.get_json()
     print(data['img'])
 
-    #un comment the below for prediction using the model
-    
+    # un comment the below for prediction using the model
+
     """ model = load_model('model2_resnet.h5')
     img_path = data["img"]
     print(img_path)
@@ -105,8 +104,6 @@ def example():
 
     return class_labels[pred[0]] """
     return ""
-    
-    
 
 
 if __name__ == '__main__':
